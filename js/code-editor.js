acfc516146e98c4d6356bc6dd0bf04385a4d2a1e
Ed Ball <info@aptary.com>
2014-06-20 12:59:33 +0100

    Auto format code editor

--- js/code-editor.js
+++ js/code-editor.js
@@ -1,8 +1,26 @@
+function autoFormatSelection(editor) {
+    var range = getSelectedRange(editor);
+    editor.autoFormatRange(range.from, range.to);
+    CodeMirror.commands['goPageUp'](editor);
+}
+
+function getSelectedRange(editor) {
+    return { from: editor.getCursor(true), to: editor.getCursor(false) };
+}
+
 $(".js-code-pattern").each(function(){
 	var myCodeMirror = CodeMirror.fromTextArea($(this)[0], {
 	    lineNumbers: false,
-	    mode: "xml",
+	    mode: "text/html",
 	    htmlMode: true,
-	    readOnly: true
+	    readOnly: true,
+	    indentWithTabs: true,
+	    indentUnit: 4,
+	    autoClearEmptyLines: true,
+        lineWrapping: true,
 	 });
+
+    CodeMirror.commands["selectAll"](myCodeMirror);
+    autoFormatSelection(myCodeMirror);
+
 });
