94a75d84129edda4df71856b74cf41852ec5b39b
Harry Parton <hparton@hotmail.co.uk>
2014-09-16 11:28:20 +0100

    Reduce the markup outputted by tabs JS, automatically adds tabs based on highlight code.

--- js/boostrap.js
+++ js/boostrap.js
@@ -3,12 +3,39 @@ $(document).ready(function() {
 	
 	$('.markup').each(function(){
 
+
 		var tabNumData = 1;
 		var MarkupNumData = 1;
 
-		$(this).children('.tabs').children('li').each(function(){
-			$(this).attr( "data-num", tabNumData);
+		// Add in the tabs container.
+		$(this).prepend('<ul class="tabs"></ul>');
+
+
+		$(this).children('div').each(function(){
 			
+			// For each div add in a data attribute.
+			$(this).attr( "data-tab", MarkupNumData);
+
+			// Make the first div active.
+			if ( MarkupNumData == 1 ) {
+				$(this).addClass('active');
+			}
+
+			// Get the class that jekyll adds from its highlighting function.
+			var tabType = $(this).find('code').attr('class');
+
+			// Append each tab into the tabs container.
+			$(this).parent().children('.tabs').append('<li>'+tabType+'</li>');
+
+			MarkupNumData++;
+		});
+
+		$(this).children('.tabs').children('li').each(function(){
+
+			// Add in a data attribute equal to the div's data attribute.
+			$(this).attr( "data-tab", tabNumData);
+			
+			// Make the first tab active.
 			if ( tabNumData == 1 ) {
 				$(this).addClass('active');
 			}
@@ -16,29 +43,21 @@ $(document).ready(function() {
 			tabNumData++;
 		});
 
-		$(this).children('div').each(function(){
-			$(this).attr( "data-num", MarkupNumData);
-
-			if ( MarkupNumData == 1 ) {
-				$(this).addClass('active');
-			}
-
-			MarkupNumData++;
-		});
 
 	});
 
 	$('.markup .tabs li').on('click', function() {
 
-		var tabNumber = $(this).data("num");
-		var divNumber = $(this).closest('.markup').children('div[data-num="' + tabNumber + '"]');
+		// Swap the active classes around using the numbers data attributes.
 
-		$(this).addClass('active');
-		$(this).siblings().removeClass('active');
-		$(divNumber).addClass('active');
-		$(divNumber).siblings().removeClass('active');
+		var $tabNumber = $(this).data("tab");
+		var $divNumber = $(this).closest('.markup').children('div[data-tab="' + $tabNumber + '"]');
+
+		$(this).addClass('active').siblings().removeClass('active');
+		$($divNumber).addClass('active').siblings().removeClass('active');
 
 	});
 
 
 });
+
