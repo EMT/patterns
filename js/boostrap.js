5320620274cbab6051a96d51b00c34979c49a6de
Harry Parton <hparton@hotmail.co.uk>
2014-09-16 10:32:04 +0100

    Improve new tabs, allow for multiple on a page and remove markup

--- js/boostrap.js
+++ js/boostrap.js
@@ -1,22 +1,42 @@
 $(document).ready(function() {
 
-	var i = 0;
+	
+	$('.markup').each(function(){
 
-	$('.markup .tabs li').each(function(){
-		$(this).data("num", i);
-		i++;
+		var tabNumData = 1;
+		var MarkupNumData = 1;
+
+		$(this).children('.tabs').children('li').each(function(){
+			$(this).attr( "data-num", tabNumData);
+			
+			if ( tabNumData == 1 ) {
+				$(this).addClass('active');
+			}
+
+			tabNumData++;
+		});
+
+		$(this).children('div').each(function(){
+			$(this).attr( "data-num", MarkupNumData);
+
+			if ( MarkupNumData == 1 ) {
+				$(this).addClass('active');
+			}
+
+			MarkupNumData++;
+		});
+
 	});
 
 	$('.markup .tabs li').on('click', function() {
-		var name = $(this).html();
-		var nameClass = '.' + name;
 
+		var tabNumber = $(this).data("num");
+		var divNumber = $(this).closest('.markup').children('div[data-num="' + tabNumber + '"]');
 
 		$(this).addClass('active');
 		$(this).siblings().removeClass('active');
-
-		$(nameClass).addClass('active');
-		$(nameClass).siblings().removeClass('active');
+		$(divNumber).addClass('active');
+		$(divNumber).siblings().removeClass('active');
 
 	});
 
